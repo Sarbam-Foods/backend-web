7ebc8e0509fdf9efed5c9efabd1a2c178830bafc
riwaj9786 <riwajbhurtel9786@gmail.com>
2025-01-14 09:59:31 +0545

    New Database

--- sarbam_mail/sarbam_mail/urls.py
+++ sarbam_mail/sarbam_mail/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -56,3 +58,6 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
