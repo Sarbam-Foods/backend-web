e52792d58ded3952f26b8efd6e46f31efd834269
Sarbam-Foods <sarbamfoods@gmail.com>
2025-01-29 15:01:46 +0545

    hotfix:redis host fix

--- sarbam_mail/sarbam_mail/settings.py
+++ sarbam_mail/sarbam_mail/settings.py
@@ -177,7 +177,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
